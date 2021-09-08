4bfd12c936bb6a4ddb735111a3472a302d93a2d6
Kirk Tolliver <KirkTolliver28@gmail.com>
2021-09-08 18:28:01 -0500

    Adding madlib game with fgets example

--- calcu.c
+++ calcu.c
@@ -6,16 +6,7 @@
 */
 int main()
 {
-	double num1;
-	double num2;
+	char color[20];
 
-	printf("Enter your first number: ");
-	scanf("%lf", &num1);
-	
-	printf("Enter your second number: ");
-	scanf("%lf", &num2);
 
-	printf(" Total: %lf", num1 + num2);
-
-	return 0;
 }
