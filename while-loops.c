07f223c60a1088b16e9c38cbfdcaf4be6f562669
Kirk Tolliver <KirkTolliver28@gmail.com>
2021-09-09 23:24:49 -0500

    Added while-loop

---
+++ while-loops.c
@@ -0,0 +1,19 @@
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Description: Simple format statements
+ * Author: Kirk Tolliver
+*/
+
+	 int main()
+{
+	int index = 1;
+	while(index <=5){
+		printf("%d\n", index);
+		index++;
+	}
+	return 0;
+}
+
