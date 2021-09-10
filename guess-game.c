4218b8eb566532d8ff6be36c24134ab84f03b8bb
Kirk Tolliver <KirkTolliver28@gmail.com>
2021-09-10 12:13:12 -0500

    Refactored guessing game to limit the amount of guesses

--- guess-game.c
+++ guess-game.c
@@ -6,13 +6,25 @@
 */
 int main()
 {
-  int secretNumber =5;
 	int guess;
+  int secretNumber =5;
+	int guessCount = 0;
+	int guessLimit = 3;
+	int outOfGuesses = 0;
 
-	while(guess != secretNumber){
-		printf("Enter a number: ");
-		scanf("%d", &guess);
+	while(guess != secretNumber && outOfGuesses ==0 ){
+		if(guessCount < guessLimit){
+				printf("Enter a number: ");
+				scanf("%d", &guess);
+				guessCount++;
+		} else {
+				outOfGuesses = 1;
+		}
 }
-	printf("You win!");
-	return 0;
+	if(outOfGuesses ==1){
+		printf("Out of guesses");
+	}esle {
+		printf("You win!");
+	}
+			return 0;
 }
