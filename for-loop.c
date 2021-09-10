3f9d49528bd225d4102c5b4a3a48e484879446e4
Kirk Tolliver <KirkTolliver28@gmail.com>
2021-09-10 12:53:14 -0500

    Added for-loop example for C

---
+++ for-loop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Description: Simple format statements
+ * Author: Kirk Tolliver
+*/
+int main()
+{
+	int i = 0;
+
+	while( i <= 5){
+				printf("%d\n", i);
+				i++;
+		}
+	int j;
+
+	for(j =1; j <= 20; j++){
+		printf("%d\n", j);
+	}
+			return 0;
+}
